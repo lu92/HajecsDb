99d84fff962f0dcfa2ad6cddb1cce8627785ac1d
lu92 <biklucjan@gmail.com>
2017-05-03 23:57:44 +0200

    [US1018][Transactions] - additional data of Transactions

--- src/main/java/org/hajecsdb/graphs/transactions/TransactionManager.java
+++ src/main/java/org/hajecsdb/graphs/transactions/TransactionManager.java
@@ -1,25 +1,38 @@
 package org.hajecsdb.graphs.transactions;
 
-import org.hajecsdb.graphs.storage.GraphStorage;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.UUID;
 
 public class TransactionManager {
     private int defaultTimeout;
     private int maximumTimeout;
 
+    // zazadza transakcjami
+    // poziom izolacji transakcji
 
     public Transaction createTransaction() {
         return new Transaction() {
+            private long transactionId = generateUniqueId();
+            private boolean performed = false;
             private TransactionStatus transactionStatus = new TransactionStatus();
             private TransactionScope transactionScope = new TransactionScope();
 
             @Override
-            public boolean commit() {
-                return false;
+            public long getId() {
+                return transactionId;
             }
 
             @Override
-            public boolean rollback() {
-                return false;
+            public TransactionStatus commit() {
+                performed = true;
+                return null;
+            }
+
+            @Override
+            public TransactionStatus rollback() {
+                performed = true;
+                return null;
             }
 
             @Override
@@ -30,6 +43,31 @@ public class TransactionManager {
             @Override
             public TransactionScope getScope() {
                 return transactionScope;
+            }
+
+            @Override
+            public boolean isPerformed() {
+                return performed;
+            }
+
+
+            /**
+             * Gnereate unique ID from UUID in positive space
+             * @return long value representing UUID
+             */
+            private Long generateUniqueId()
+            {
+                long val = -1;
+                do
+                {
+                    final UUID uid = UUID.randomUUID();
+                    final ByteBuffer buffer = ByteBuffer.wrap(new byte[16]);
+                    buffer.putLong(uid.getLeastSignificantBits());
+                    buffer.putLong(uid.getMostSignificantBits());
+                    final BigInteger bi = new BigInteger(buffer.array());
+                    val = bi.longValue();
+                } while (val < 0);
+                return val;
             }
         };
     }
