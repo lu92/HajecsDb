8ce54f35850ffabf08e1fc7be9854c885382df36
lu92 <biklucjan@gmail.com>
2017-05-14 01:36:27 +0200

    [US1018][Transactions] - test cases of transactions which work on same node

--- src/test/java/HajecsDb/unitTests/transactions/IsolationTransactionTest.java
+++ src/test/java/HajecsDb/unitTests/transactions/IsolationTransactionTest.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.hajecsdb.graphs.core.PropertyType.DOUBLE;
 import static org.hajecsdb.graphs.core.PropertyType.LONG;
 import static org.hajecsdb.graphs.core.PropertyType.STRING;
 
@@ -177,15 +178,6 @@ public class IsolationTransactionTest {
                 createdNodes.add(bob);
                 createdNodes.add(frank);
 
-                // after first transaction commitment, second one should contain all nodes
-//                transactionalGraphService.context(tran2).getAllNodes().forEach(searchedNode ->
-//                {
-//                    Node equivalentNode = createdNodes.stream().
-//                            filter(node -> node.getProperty("name").get().equals(searchedNode.getProperty("name").get()))
-//                            .findFirst().get();
-//                    assertThat(nodeComparator.isSame(searchedNode, equivalentNode)).isTrue();
-//                });
-
                 // after commitment tran2, transactionalGraphService should contain alice gina bob and frank's nodes
                 transactionalGraphService.context(tran2).commit();
                 System.out.println(Thread.currentThread().getName() + " tran2 committed");
@@ -193,9 +185,6 @@ public class IsolationTransactionTest {
                 waiter.resume();
             }
         });
-
-//        // for now graph should be empty - nothing was committed
-//        assertThat(transactionalGraphService.getAllPersistentNodes()).isEmpty();
 
         // when
         user1.start();
@@ -210,103 +199,198 @@ public class IsolationTransactionTest {
         // then
         assertThat(transactionalGraphService.getAllPersistentNodes()).hasSize(4);
 
-//        transactionalGraphService.getAllPersistentNodes().forEach(searchedNode ->
-//        {
-//            Node equivalentNode = createdNodes.stream().
-//                    filter(node -> node.getProperty("name").get().equals(searchedNode.getProperty("name").get()))
-//                    .findFirst().get();
-//            assertThat(nodeComparator.isSame(searchedNode, equivalentNode)).isTrue();
-//        });
+        transactionalGraphService.getAllPersistentNodes().forEach(searchedNode ->
+        {
+            Node equivalentNode = createdNodes.stream().
+                    filter(node -> node.getProperty("name").get().equals(searchedNode.getProperty("name").get()))
+                    .findFirst().get();
+            assertThat(nodeComparator.isSame(searchedNode, equivalentNode)).isTrue();
+        });
     }
 
 
-//    @Test
-//    public void updateSamePropertyOfNodeInTwoTransactionsThenCommit() throws InterruptedException, TimeoutException {
-//
-//
-//        // given
-//        Session session = sessionPool.createSession();
-//        session.setTransactionManager(transactionManager);
-//        TransactionalGraphService transactionalGraphService = new TransactionalGraphService();
-//
-//        // this transaction prepares state of graph
-//        Transaction transaction = session.beginTransaction();
-//        transactionalGraphService.context(transaction)
-//                .createNode(new Label("Person"), new Properties()
-//                        .add(new Property("name", STRING, "Alice"))
-//                        .add(new Property("age", LONG, 25l)));
-//
-//        transactionalGraphService.context(transaction).commit();
-//
-//        // validate content of graph
-//        assertThat(transactionalGraphService.getAllPersistentNodes()).hasSize(1);
-//
-//        Waiter waiter = new Waiter();
-//
-//        Transaction tran1 = session.beginTransaction();
-//        Transaction tran2 = session.beginTransaction();
-//
-//        Thread user1 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//
-//                System.out.println("Thread " + Thread.currentThread().getName() + " performing tran1");
-//
-//                // tran1 sets age of alice to 20
-//                transactionalGraphService.context(tran1).setPropertyToNode(1, new Property("age", LONG, 20l));
-//                waiter.assertEquals(transactionalGraphService.context(tran1).getNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 20l));
-//
-//                // validate age of alice in tran1
-//                waiter.assertEquals(transactionalGraphService.context(tran1).getNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 20l));
-//
-//                try {
-//                    Thread.sleep(200);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                // validate age of alice in committed graph
-//                waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 25l));
-//
-//                waiter.resume();
-//            }
-//        });
-//
-//        Thread user2 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("Thread " + Thread.currentThread().getName() + " performing tran2");
-//
-//                // tran2 sets age of alice to 30
-//                transactionalGraphService.context(tran2).setPropertyToNode(1, new Property("age", LONG, 30l));
-//                waiter.assertEquals(transactionalGraphService.context(tran2).getNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 30l));
-//
-//                waiter.assertEquals(transactionalGraphService.context(tran2).getNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 30l));
-//
-//                // validate age of alice in committed graph
-//                waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 25l));
-//
-//                waiter.resume();
-//            }
-//        });
-//
-//
-//        user1.start();
-//        waiter.await(1000);
-//        user2.start();
-//        waiter.await(1000);
-//
-//
-//        transactionalGraphService.context(tran1).commit();
-//        transactionalGraphService.context(tran2).commit();
-//
-//        // validate age of alice in committed graph
-//        assertThat(transactionalGraphService.getPersistentNodeById(1).get().getProperty("age").get())
-//                .isEqualTo(new Property("age", LONG, 25l));
-//    }
+    @Test
+    public void updateSamePropertyOfNodeSecondTransactionShouldBeBlocked() throws InterruptedException, TimeoutException {
 
+        // given
+        Session session = sessionPool.createSession();
+        session.setTransactionManager(transactionManager);
+        TransactionalGraphService transactionalGraphService = new TransactionalGraphService();
+
+        // this transaction prepares state of graph
+        Transaction transaction = session.beginTransaction();
+        transactionalGraphService.context(transaction)
+                .createNode(new Label("Person"), new Properties()
+                        .add(new Property("name", STRING, "Alice"))
+                        .add(new Property("age", LONG, 25l)));
+
+        transactionalGraphService.context(transaction).commit();
+
+        // validate content of graph
+        assertThat(transactionalGraphService.getAllPersistentNodes()).hasSize(1);
+
+        Waiter waiter = new Waiter();
+
+
+        Thread user1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println("Thread " + Thread.currentThread().getName() + " performing tran1");
+                Transaction tran1 = session.beginTransaction();
+
+                // tran1 sets age of alice to 20
+                transactionalGraphService.context(tran1).setPropertyToNode(1, new Property("age", LONG, 20l));
+                waiter.assertEquals(transactionalGraphService.context(tran1).getNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 20l));
+
+                // validate age of alice in tran1
+                waiter.assertEquals(transactionalGraphService.context(tran1).getNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 20l));
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // validate age of alice in committed graph
+                waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 25l));
+
+                waiter.resume();
+            }
+        });
+
+        Thread user2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Transaction tran2 = session.beginTransaction();
+                System.out.println("Thread " + Thread.currentThread().getName() + " performing tran2");
+
+                // tran2 sets age of alice to 30
+                transactionalGraphService.context(tran2).setPropertyToNode(1, new Property("age", LONG, 30l));
+                waiter.assertEquals(transactionalGraphService.context(tran2).getNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 30l));
+
+                waiter.assertEquals(transactionalGraphService.context(tran2).getNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 30l));
+
+                // validate age of alice in committed graph
+                waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("age").get(), new Property("age", LONG, 25l));
+
+                waiter.resume();
+            }
+        });
+
+
+        user1.start();
+        waiter.await(1000);
+        Thread.sleep(10);
+
+        // when
+        user2.start();
+        try {
+            waiter.await(100);
+        } catch (TimeoutException e) {
+            // then
+            assertThat(e.getMessage()).isEqualTo("Test timed out while waiting for an expected result");
+        }
+
+    }
 
 
     // concurentupdateOfSameNode_second should wait until forist one will commit
+
+    @Test
+    public void twoTransactionsTryToUpdateSameNodeSecondTransactionShouldBeBlockedAndReleasedAfterFirstCommitTest() throws TimeoutException, InterruptedException {
+        // given
+        Session session = sessionPool.createSession();
+        session.setTransactionManager(transactionManager);
+        TransactionalGraphService transactionalGraphService = new TransactionalGraphService();
+
+        // this transaction prepares state of graph
+        Transaction transaction = session.beginTransaction();
+        Node alice = transactionalGraphService.context(transaction)
+                .createNode(new Label("Person"), new Properties()
+                        .add(new Property("name", STRING, "Alice"))
+                        .add(new Property("age", LONG, 25l))
+                        .add(new Property("salary", DOUBLE, 3000.00)));
+
+        transactionalGraphService.context(transaction).commit();
+
+        // validate content of graph
+        assertThat(transactionalGraphService.getAllPersistentNodes()).hasSize(1);
+
+        Waiter waiter = new Waiter();
+
+
+        Thread user1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println("Thread " + Thread.currentThread().getName() + " performing tran1");
+                Transaction tran1 = session.beginTransaction();
+
+                // tran1 decreases alice's salaray of 100
+                double salary = (Double) transactionalGraphService.context(tran1).getNodeById(alice.getId()).get().getProperty("salary").get().getValue();
+                salary -= 100;
+                transactionalGraphService.context(tran1).setPropertyToNode(1, new Property("salary", DOUBLE, salary));
+
+
+                // validate salary of alice in tran1
+                waiter.assertEquals(transactionalGraphService.context(tran1).getNodeById(1).get().getProperty("salary").get(), new Property("salary", DOUBLE, 2900.00));
+
+                // validate age of alice in committed graph
+                waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("salary").get(), new Property("salary", DOUBLE, 3000.00));
+
+                try {
+                    Thread.sleep(50);  // to simulate user's work
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                transactionalGraphService.context(tran1).commit();
+
+                // validate age of alice after commit
+                waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("salary").get(), new Property("salary", DOUBLE, 2900.00));
+
+                waiter.resume();
+            }
+        });
+
+        Thread user2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Transaction tran2 = session.beginTransaction();
+                System.out.println("Thread " + Thread.currentThread().getName() + " performing tran2");
+
+                // tran2 decreases alice's salaray of 200
+                double salary = (Double) transactionalGraphService.context(tran2).getNodeById(alice.getId()).get().getProperty("salary").get().getValue();
+                salary -= 200;
+                transactionalGraphService.context(tran2).setPropertyToNode(1, new Property("salary", DOUBLE, salary));
+                waiter.assertEquals(transactionalGraphService.context(tran2).getNodeById(1).get().getProperty("salary").get(), new Property("salary", DOUBLE, 2700.00));
+
+                // validate age of alice in committed graph
+                waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("salary").get(), new Property("salary", DOUBLE, 2900.00));
+
+                transactionalGraphService.context(tran2).commit();
+
+                // validate age of alice after commit
+                waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("salary").get(), new Property("salary", DOUBLE, 2700.00));
+
+                waiter.resume();
+            }
+        });
+
+
+        user1.start();
+        waiter.await(100);
+
+        // first user should start his work
+        Thread.sleep(5);
+
+        // when
+        user2.start();
+        waiter.await(100);
+
+        // validate committed node
+        waiter.assertEquals(transactionalGraphService.getPersistentNodeById(1).get().getProperty("salary").get(), new Property("salary", DOUBLE, 2700.00));
+    }
 
 }
